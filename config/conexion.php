ce8c663d600be9aa86c67e156baa0ef414d4d194
H-S <H-S@users.noreply.github.com>
2022-07-26 17:30:03 -0600

    Se modificó la conexión

--- config/conexion.php
+++ config/conexion.php
@@ -4,16 +4,16 @@ protected $dbh;
 
 protected function Conexion(){
 try {
-$conectar =$this-> $dbh = new PDO("mysql:host=20.216.41.245;dbname=g1_20","G7_20", "a60CZ6ku");
+$conectar =$this-> dbh = new PDO("mysql:host=20.216.41.245;dbname=g7_20","G7_20", "a60CZ6ku");
 return $conectar;
 } catch(Exception $e){
-print "!Error BD!:".$e->getmessager()."<br/>";
+print "!Error BD!:".$e->getMessage()."<br/>";
 die();
 }
 
 }
-public function set_name(){
-    return $this->dbh->query("SET NAME 'utf8'");
+public function set_names(){
+    return $this->dbh->query("SET NAMES 'utf8'");
 }
 
 
