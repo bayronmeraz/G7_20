f3f66b11668c187bd0acf64572f9d40e333671e1
H-S <H-S@users.noreply.github.com>
2022-07-27 15:05:28 -0600

    Se modificó archivo Estudiante

--- models/Estudiante.php
+++ models/Estudiante.php
@@ -41,16 +41,16 @@
             public function update_estudiante($numeroAlumno, $nombre, $apellidos, $fechaNacimiento, $direccion, $altura, $carrera){
                 $conectar = parent::conexion();
                 parent::set_names();
-                $sql = "UPDATE estudiante SET numeroAlumno= ?, nombre= ?, apellidos= ?, fechaNacimiento= ?, direccion= ?, altura= ?, carrera= ?
+                $sql = "UPDATE estudiante SET nombre= ?, apellidos= ?, fechaNacimiento= ?, direccion= ?, altura= ?, carrera= ?
                 WHERE numeroAlumno = ?;";
                 $sql=$conectar->prepare($sql);
-                $sql->bindValue(1,$numeroAlumno);
-                $sql->bindValue(2,$nombre);
-                $sql->bindValue(3,$apellidos);
-                $sql->bindValue(4,$fechaNacimiento);
-                $sql->bindValue(5,$direccion);
-                $sql->bindValue(6,$altura);
-                $sql->bindValue(7,$carrera);
+                $sql->bindValue(1,$nombre);
+                $sql->bindValue(2,$apellidos);
+                $sql->bindValue(3,$fechaNacimiento);
+                $sql->bindValue(4,$direccion);
+                $sql->bindValue(5,$altura);
+                $sql->bindValue(6,$carrera);
+                $sql->bindValue(7,$numeroAlumno);
                 $sql->execute();
                 return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
             }
